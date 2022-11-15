6c71e89b82ee4866745855748f1391698c84d78e
melaniekuo1 <kuo.melanie@gmail.com>
2022-11-15 09:26:17 -0800

    trying to run treemap

--- P2/B3.R
+++ P2/B3.R
@@ -6,4 +6,3 @@ data_table <- women_stem %>% group_by(Major_category)
 data_table <- aggregate(data_table$Women, by = list(data_table$Major_category), FUN = mean)
 colnames(data_table)[1] <- "Major Catagories"
 colnames(data_table)[2] <- "Total Number of Women"
-view(data_table)
