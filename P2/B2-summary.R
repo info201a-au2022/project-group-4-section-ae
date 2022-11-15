721a0ac4625a2a70279607a2d6476dcec2362fdc
aronaacho <aronacho@uw.edu>
2022-11-14 23:06:39 -0800

    Summary

--- P2/B2-summary.R
+++ P2/B2-summary.R
@@ -1,34 +1,25 @@
 # summary_info.R 
 # A source file that takes in a dataset and returns a list of info about it:
 
-women_stem <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ae/main/data/women-stem.csv")
+library(dplyr)
 
+women_stem <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ae/main/data/women-stem.csv")
+View()
 summary_info_max <-list()
-summary_info_max$num_observations <- nrow(women_stem)
 summary_info_max$Median_max_value <- women_stem %>%
   filter(Median == max(Median, na.rm = T)) %>%
   select(Median)
 
 summary_info_min <- list()
-summary_info_min$num_observations <- nrow(women_stem)
 summary_info_min$Median_min_value <- women_stem %>%
   filter(Median == min(Median, na.rm = T)) %>%
   select(Median)
 
-summary_info_mean <- list()
-summary_info_mean$num_observations <- nrow(women_stem)
-summary_info_mean$Median_mean_value <- women_stem %>%
-  filter(Median == mean(Median, na.rm = T)) %>%
-  select(Median)
+summary_info_mean <- women_stem %>% 
+  summarise(meansal = mean(women_stem$Median))
 
-summary_info_median <- list()
-summary_info_median$num_observations <- nrow(women_stem)
-summary_info_median$Median_median_value <- women_stem %>%
-  filter(Median == median(Median, na.rm = T)) %>%
-  select(Median)
+summary_info_median <- women_stem %>% 
+  summarise(mediansal = median(women_stem$Median))
 
-summary_info_range <- list()
-summary_info_range$num_observations <- nrow(women_stem)
-summary_info_range$Median_range_value <- women_stem %>%
-  filter(Median == (max(Median, na.rm = T)) - min(Median, na.rm = T)) %>%
-  select(Median)
+summary_info_range <- women_stem %>% 
+  summarize((rangesal = ((max(women_stem$Median))-(min(women_stem$Median)))))
