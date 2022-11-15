19511870b250b35e9c40bea9e9f240e63cd8a279
melaniekuo1 <kuo.melanie@gmail.com>
2022-11-14 21:38:54 -0800

    restoring deleted files

--- P2/B.4.R
+++ P2/B.4.R
@@ -3,10 +3,7 @@ library(dplyr)
 
 women_stem <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ae/main/data/women-stem.csv")
 
-arranged_stem <- women_stem %>% 
-  mutate(desc(ShareWomen))
-
-women_stem_plot <- ggplot(arranged_stem) +
+women_stem_plot <- ggplot(women_stem) +
   ggtitle("Shares of Total Women in STEM Majors") +
   geom_col(mapping = aes(x = ShareWomen, y = Major)) +
   labs(x = "Women as Share of Total Major Population", y = "Majors") +
@@ -16,5 +13,5 @@ women_stem_plot <- ggplot(arranged_stem) +
   included the highest share of women, while majors in Engineering (such as Mining and Mineral Engineering) 
   was the category with the lowest share of women, demonstrating an area where STEM gender diversity needs to improve.") +
   theme(plot.caption.position = "plot",
-        plot.caption = element_text(hjust = 0)) 
+        plot.caption = element_text(hjust = 0))
 women_stem_plot
