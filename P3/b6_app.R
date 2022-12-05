a5b26c923b870cc471eae93aac5646e856c98bbd
IreneOnionring <irenejo@uw.edu>
2022-12-05 02:49:32 -0800

    irene

---
+++ P3/b6_app.R
@@ -0,0 +1,6 @@
+library("shiny")
+
+source("b6_ui.R")
+source("b6_server.R")
+
+shinyApp(ui = ui, server = server)
---
+++ P3/b6_ui.R
@@ -0,0 +1,18 @@
+library("shiny")
+library("ggplot2")
+
+range_input <- sliderInput("range", "Range of Umeployment Rate:",
+                            min = 0.00, max = 0.15, value = 0.15)
+
+major_input <- selectInput("major_catagories", "majors", multiple = TRUE, 
+                           choices = all_ages$Major_category)
+
+ui <- fluidPage(
+  
+  h1("College Majors and Unemployment Rate Interactive Visualization"),
+  
+  range_input, 
+  major_input,
+  
+  plotOutput("plot")
+)
