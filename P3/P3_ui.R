b05349bfd1cbafa0c9e601787bb6fbaf76e773a7
IreneOnionring <irenejo@uw.edu>
2022-12-05 10:26:27 -0800

    irene

--- P3/P3_ui.R
+++ P3/P3_ui.R
@@ -1,1 +1,23 @@
 # ui P3
+
+
+library("shiny")
+library("plotly")
+library("ggplot2")
+
+# BASIC FORMAT FOR TABS AND PAGES
+
+all_ages_data <- mainPanel(
+  p("this is a test for para")
+)
+
+
+intro_tab_panel <- tabPanel("Tab Introduction", 
+                            titlePanel("title for page"),
+                            intro_main_content)
+
+
+# MAKE SURE ONCE YOURE DONE WITH YOUR TAB TO INCLUDE IT IN THE NAV BAR PAGE
+
+ui <- navbarPage("Title for Tabs",
+                 intro_tab_panel)
