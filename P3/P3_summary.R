316154d0d08b6d635089c5b9948cd061d2d564ad
IreneOnionring <irenejo@uw.edu>
2022-12-06 20:31:18 -0800

    irene

--- P3/P3_summary.R
+++ P3/P3_summary.R
@@ -5,12 +5,11 @@ summary_tab <- tabPanel(
   "Summary",
   titlePanel("Summary Takeaways"),
   h2("Majors and Unemployment"),
-  p("Rates of employment after graduation matter much to incoming students who seeks a secure path to their jobs after college. With out dataset, all ages, we have found that certain majors catagories do have much higher rates of employment after graduation, which are engineering, education and agriculture& natural resource. 
-  With significantly less unemployment rates after graudation compared to 13 other major catagories explored within the dataset, these majors hold stronger promises to future jobs. On the other side, majors such as . As teh average of unemployment
-  rate ranges around 0.075, those major catetories show a"),
+  p("Rates of employment after graduation matter much to incoming students who seeks a secure path to their jobs after college. With our dataset, all ages, we have found that certain major catagories do have much higher rates of employment after graduation, the top three being engineering, education and agriculture & natural resource. 
+    With significantly less unemployment rates after graudation compared to 13 other major catagories explored within the dataset, these majors hold stronger promises to future jobs. On the other side, majors such as arts, humanities, psychology & social works have shown greater rates of unemployments after graduation."),
   h2("Major catagories"),
   p("There are alot of majors within the eigineering catagory, of 26 engineering majors within the proven data set majors_list"),
   h2("Majors and Gender"),
-  p("lastly, final key take away is that engineering is the STEM major category with the lowest share of women, while health has the highest share of women.
+  p("Gender diversity within various majors were also topic we investigated with our data visualization, and have come to the conlcusion that engineering is the STEM major category with the lowest share of women, while health has the highest share of women.
     The top three majors with the LOWEST share of women are mechanical engineering related technologies (around 7.8%), mining and mechanical engineering (around 10.1%) and naval architecture and marine engineering (around 10.7%). the top three majors with the HIGHEST share of women are communication disorders sciences and services (around 96.7%), medical assisting services (around 92.3%), and nursing (around 89.7%) Thus out data visualizations have shown that engineering is the STEM major field that most needs to increase its gender diversity!"),
 )
--- P3/b6_server.R
+++ P3/b6_server.R
@@ -4,20 +4,17 @@ library(tidyverse)
 
 server <- function(input, output) {
   
-
-  all_ages_data <- renderPlot({
     all_ages <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ae/main/data/all-ages.csv")
   
     #all_ages <- all_ages %>%  filter(Unemployment_rate > input$range_input)
     #do I need to do this
-    all_ages <- all_ages %>% filter(Major_category %in% input$major_input) %>% group_by(Major_category, na.rm = TRUE) %>% summarise(Unemployment_rate_mean = mean(Unemployment_rate))
-  })
   
   output$plot <- renderPlot({
-    #switch to all_ages_data() when work
-    all_ages_plot <- ggplot(all_ages, mapping = aes(y = Major_category, x = Unemployment_rate)) + 
+    all_ages <- all_ages %>% filter(Major_category %in% input$major_categories) %>% group_by(Major_category)%>% summarise(Unemployment_rate_mean = mean(Unemployment_rate))
+    
+    all_ages_plot <- ggplot(all_ages, mapping = aes(x = Unemployment_rate_mean, y = Major_category)) + 
       ggtitle("Major and Unemployment Rate") + 
-      geom_violin() + 
+      geom_col() +
       labs(caption = "This chart demonstrates which catagories of college majors 
        have the greatest and least rates of Unemployment. We wanted to create a 
        data visualization that makes it easy for students to see which
